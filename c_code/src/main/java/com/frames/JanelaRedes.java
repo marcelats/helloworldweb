2a52fb6b7ee280723d55585ef9ea0d2a528f65b4
marcelats <marcelatshinzato@gmail.com>
2025-07-18 10:55:09 -0300

    Update JanelaRedes.java

--- c_code/src/main/java/com/frames/JanelaRedes.java
+++ c_code/src/main/java/com/frames/JanelaRedes.java
@@ -3,7 +3,7 @@ package com.frames;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.stream.Collectors;
-
+import java.nio.file.Paths;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
